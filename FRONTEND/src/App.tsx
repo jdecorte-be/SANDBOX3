faa4411df34d1618ff7b3cb3d5e81511e08ad3dd
ablondel19 <ablondel@student.s19.be>
2023-02-11 16:01:00 +0100

    clean

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -8,44 +8,44 @@ import { FileUpload } from './components/FileUpload';
 import { Profile } from './components/Profile';
 import axios from 'axios';
 import { Leaderboard } from './components/Leaderboard';
-//import { Gaming } from './components/Canvas';
+import { Gaming } from './components/Canvas';
 
 function App() {
-  // const click = () => {
-  //   axios
-  //     .get('http://localhost:3001/app/auth/who', {
-  //       headers: { Authorization: document.cookie },
-  //     })
-  //     .then((response) => {
-  //       console.log('Login -> ', response.data.login);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // };
-  // console.log('test');
-  // let game = new Gaming(1000, 1000);
+  const click = () => {
+    axios
+      .get('http://localhost:3001/app/auth/who', {
+        headers: { Authorization: document.cookie },
+      })
+      .then((response) => {
+        console.log('Login -> ', response.data.login);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  console.log('test');
+  let game = new Gaming(1000, 1000);
 
-  // const addLobby = () => {
-  //   game.socket.emit('CreateLobby', (data: any) => {
-  //     console.log(data);
-  //   });
-  // };
-  // const joinLobby = () => {
-  //   game.socket.emit('JoinLobby', (data: any) => {
-  //     console.log(data);
-  //   });
-  // };
-  // const leaveLobby = () => {
-  //   game.socket.emit('LeaveLobby', (data: any) => {
-  //     console.log(data);
-  //   });
-  // };
-  // const printLobby = () => {
-  //   game.socket.emit('LobbyInfo', (data: any) => {
-  //     console.log(data);
-  //   });
-  // };
+  const addLobby = () => {
+    game.socket.emit('CreateLobby', (data: any) => {
+      console.log(data);
+    });
+  };
+  const joinLobby = () => {
+    game.socket.emit('JoinLobby', (data: any) => {
+      console.log(data);
+    });
+  };
+  const leaveLobby = () => {
+    game.socket.emit('LeaveLobby', (data: any) => {
+      console.log(data);
+    });
+  };
+  const printLobby = () => {
+    game.socket.emit('LobbyInfo', (data: any) => {
+      console.log(data);
+    });
+  };
   return (
     <>
       <SignUp />
@@ -54,11 +54,11 @@ function App() {
       <Code2FA />
       <Profile />
       <Leaderboard />
-      {/* <button onClick={click}>Who</button> */}
-      {/* <button onClick={addLobby}>Create Lobby</button> */}
-      {/* <button onClick={joinLobby}>Join Lobby</button> */}
-      {/* <button onClick={leaveLobby}>Leave Lobby</button> */}
-      {/* <button onClick={printLobby}>Print Lobby</button> */}
+      {<button onClick={click}>Who</button>}
+      {<button onClick={addLobby}>Create Lobby</button>}
+      {<button onClick={joinLobby}>Join Lobby</button>}
+      {<button onClick={leaveLobby}>Leave Lobby</button>}
+      {<button onClick={printLobby}>Print Lobby</button>}
     </>
   );
 }
